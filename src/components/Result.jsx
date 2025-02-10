6eeb494c8661bf747bb30d52c89953e49933874d
Thomas Lemaire <117656819+tom77310@users.noreply.github.com>
2025-02-10 16:14:59 +0100

    correction page result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,8 +7,9 @@ const Result = ({ score, total, onRestart }) => {
 			<p className="{styles.scoreText}">
 				Score : {score} / {total}
 			</p>
-			<p className="{styles.feedbackText}"></p>
-				{score <= 4 ? "Oups, tu peux mieux faire" : score <= 7 ? "Pas mal, tu t'en sors bien !" : "Félicitations, tu es un expert !"}
+			<p className="{styles.feedbackText}">
+				{score <= 4 ? "Oups, tu peux mieux faire" : score <= 7 ? "Pas mal, tu t'en sors bien !" : "Félicitations, tu es un expert !"}	
+			</p>
 			<button className="{styles.restartButton}" onClick={onRestart}>
 				Rejouer
 			</button>
